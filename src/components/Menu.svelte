3e7943712bd23080560f0b2a1c1d7ee91607fc14
Gabriel Freitas <freitasga.01@gmail.com>
2023-09-27 21:25:41 -0300

    feat: :lipstick: fancy styles

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -89,14 +89,18 @@
   }
 </script>
 
-<nav class="flex flex-col-reverse items-center gap-4 mt-[10px] rounded-md p-2">
+<nav class="flex flex-col items-center gap-4 mt-[10px] rounded-md p-2">
   <div class="flex flex-row gap-2">
-    <select class="rounded-md text-gray-900" bind:value={algorithm}>
+    <button
+      class="w-[180px] rounded-md px-3 py-2 bg-[#36B336] text-white font-medium border border-[#6b7280]"
+      on:click={handleStart}>Start</button
+    >
+    <select class="w-[180px] rounded-md text-gray-900" bind:value={algorithm}>
       <option value={Algorithm.AStar}>A*</option>
       <option value={Algorithm.Dijkstra}>Dijkstra</option>
     </select>
     <select
-      class="rounded-md text-gray-900"
+      class="w-[180px] rounded-md text-gray-900"
       disabled={algorithm === Algorithm.Dijkstra}
       bind:value={heuristic}
     >
@@ -107,19 +111,18 @@
   </div>
   <div class="flex flex-row gap-2 text-[#fdfdfd]">
     <button
-      class="rounded-md px-3 py-2 bg-white text-gray-900 border border-[#6b7280] ring-0"
-      on:click={handleStart}>Start</button
-    >
-    <button
-      class="rounded-md px-3 py-2 bg-white text-gray-900 border border-[#6b7280] ring-0"
+      class="w-[180px] rounded-md px-3 py-2 bg-white text-gray-900 border border-[#6b806d] disabled:opacity-70"
+      disabled={$state !== State.Idle}
       on:click={handleResetPath}>Reset Path</button
     >
     <button
-      class="rounded-md px-3 py-2 bg-white text-gray-900 border border-[#6b7280] ring-0"
+      class="w-[180px] rounded-md px-3 py-2 bg-white text-gray-900 border border-[#6b7280] disabled:opacity-70"
+      disabled={$state !== State.Idle}
       on:click={handleResetWalls}>Reset Walls</button
     >
     <button
-      class="rounded-md px-3 py-2 bg-white text-gray-900 border border-[#6b7280] ring-0"
+      class="w-[180px] rounded-md px-3 py-2 bg-white text-gray-900 border border-[#6b7280] disabled:opacity-70"
+      disabled={$state !== State.Idle}
       on:click={handleResetStartAndGoal}>Reset Start and Goal</button
     >
   </div>
